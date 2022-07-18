8910d04b8cf8c3794f445e79c2f3f39a6ff6ea0a
unknown <susandu924@gmail.com>
2022-07-18 14:57:34 -0500

    Exercises angular lesson 1 first commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/65529753_620649305095788_7005592143725068288_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=HFWzomuP1WgAX-YBhA4&_nc_oc=AQn7CbO-GOK3gZBlbKXo6sOCQX33JPayU6yU_4tzwrAsCbia08lFJMm7-kP57lUcvpw&_nc_ht=scontent-ort2-1.xx&oh=00_AT_dVe-w68Bwqu5ekkq0iPOUnGVQiVxk1KxwFpxT7HcX4g&oe=62FA3F05';
   image3 = '';
 
   constructor() { }
