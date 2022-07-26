6f06be076ef84406779697e049b49c0cd36d4c18
unknown <susandu924@gmail.com>
2022-07-25 22:02:08 -0500

    saved to be safe

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries', 'Exercise'];
+   finishedChores = ['Clean bathroom'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
@@ -17,3 +17,4 @@ export class ChoresComponent implements OnInit {
    }
 
 }
+// 'Clean bathroom', 'Do laundry', 'Fold clothes''Clean bathroom'
--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -8,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
    skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-   alternateColor: string = 'black';
-   bulletType: string = 'A';
+   alternateColor: string = 'purple';
+   bulletType: string = '1';
    changeColor: boolean = true;
-
    constructor() { }
 
    ngOnInit() { }
