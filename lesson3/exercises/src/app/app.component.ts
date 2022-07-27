ccc1487afac0d431f26aae16e2d4306c59921af1
unknown <susandu924@gmail.com>
2022-07-27 15:01:06 -0500

    lesson3 exercise works!

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,8 +12,15 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
-  takeOffEnabled: boolean = false;
+  takeOffEnabled: boolean = true;
 
+gutterCheck (width, height) {
+  if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+  this.color = 'orange';
+  } else {
+    this.color = 'blue';
+  }
+}
 
 handleTakeOff() {
   let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
@@ -22,47 +29,56 @@ handleTakeOff() {
       this.height = 10000;
       this.width = 0;
       this.message = 'Shuttle in flight.';
+      this.takeOffEnabled = false;
     }
 }
 
-handleLanding() {
+handleLanding(rocketImage) {
   let result = window.confirm("The shuttle is landing. Landing gear engaged.")
     if (result) {
       this.color = 'green';
       this.height = 0;
       this.message = "The shuttle has landed.";
+      this.takeOffEnabled = true;
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0';
     }
 }
 
-missionAbort() {
+missionAbort(rocketImage) {
   let result = window.confirm("Mission abort.");
     if (result) {
       this.color = 'green';
       this.height = 0;
       this.message = "Mission aborted.";
+      this.takeOffEnabled = true;
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0';
     }
 }
 
 moveRocket(rocketImage, direction) {
-   if (direction === 'right') {
-      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+    if 
+      (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 100 + 'px';
       rocketImage.style.left = movement;
       this.width = this.width + 10000;
-   } else
-   if (direction === 'left') {
-    let movement =  parseInt(rocketImage.style.right) + 10 + 'px';
-    rocketImage.style.right = movement;
-    this.width = this.width + 10000;
-   } else
-   if (direction === 'up') {
-    let movement =  parseInt(rocketImage.style.right) + 10 + 'px';
-    rocketImage.style.right = movement;
-    this.width = this.height + 10000;
-   }
-   else (direction === 'down') ;{
-    let movement =  parseInt(rocketImage.style.right) - 10 + 'px';
-    rocketImage.style.right = movement;
+    } else if 
+      (direction === 'left') {
+      let movement =  parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    } else if 
+    (direction === 'down') {
+    let movement =  parseInt(rocketImage.style.bottom) - 10 + 'px';
+    rocketImage.style.bottom = movement;
     this.width = this.height - 10000;
-   }
+    } else {
+    let movement =  parseInt(rocketImage.style.bottom) + 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.width = this.height + 10000;
+    }
+  this.gutterCheck(this.width, this.height);
 }
 }
+// { (direction === 'up') ;
